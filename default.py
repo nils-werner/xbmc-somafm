decce68e247cc3c6463d36b9d76e39a7c85c1505
Nils Werner <nils.werner@gmail.com>
2013-07-21 22:56:36 +0200

    4-line indenting

--- default.py
+++ default.py
@@ -7,7 +7,7 @@ __addonid__ = "plugin.audio.somafm"
 __version__ = "0.0.2"
 
 def log(msg):
-  print "[PLUGIN] '%s (%s)' " % (__addon__, __version__) + str(msg)
+    print "[PLUGIN] '%s (%s)' " % (__addon__, __version__) + str(msg)
 
 log("Initialized!")
 log(sys.argv)
@@ -19,22 +19,21 @@ handle = int(sys.argv[1])
 query = sys.argv[2]
 
 def getHeaders(withReferrer=None):
-  headers = {}
-  headers['User-Agent'] = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3'
-  if withReferrer:
-    headers['Referrer'] = withReferrer
-  return headers
+    headers = {}
+    headers['User-Agent'] = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3'
+    if withReferrer:
+        headers['Referrer'] = withReferrer
+    return headers
 
 def getHTMLFor(url, withData=None, withReferrer=None):
-  url = rootURL + url
-  log("Get HTML for URL: " + url)
-  req = urllib2.Request(url, withData, getHeaders(withReferrer))
-  response = urllib2.urlopen(req)
-  data = response.read()
-  response.close()
-  return data
-  
-  
+    url = rootURL + url
+    log("Get HTML for URL: " + url)
+    req = urllib2.Request(url, withData, getHeaders(withReferrer))
+    response = urllib2.urlopen(req)
+    data = response.read()
+    response.close()
+    return data
+
 def addEntries():
     somaXML = getHTMLFor(url="channels.xml")
     channelsContainer = ET.fromstring(somaXML)
@@ -52,7 +51,6 @@ def addEntries():
             handle=handle,
             url=url,
             listitem=li)
-
 
 addEntries()
 xbmcplugin.endOfDirectory(handle)
